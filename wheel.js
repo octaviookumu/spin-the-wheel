6808885e7bbf58309aee5c96a991a6e9f2b9860c
Octavian Okumu <48207524+octaviookumu@users.noreply.github.com>
2020-12-04 21:53:02 +0300

    Create wheel.js

---
+++ wheel.js
@@ -0,0 +1,318 @@
+// Confetti code starts
+    // global variables
+    let prize = document.getElementById('prize');
+    prize.innerHTML = "";
+    const confetti = document.getElementById('confetti');
+    const confettiCtx = confetti.getContext('2d');
+    let container, confettiElements = [], clickPosition;
+
+    // helper
+    rand = (min, max) => Math.random() * (max - min) + min;
+
+    // params to play with
+    const confettiParams = {
+        // number of confetti per "explosion"
+        number: 70,
+        // min and max size for each rectangle
+        size: {x: [5, 20], y: [10, 18]},
+        // power of explosion
+        initSpeed: 25,
+        // defines how fast particles go down after blast-off
+        gravity: 0.65,
+        // how wide is explosion
+        drag: 0.08,
+        // how slow particles are falling
+        terminalVelocity: 6,
+        // how fast particles are rotating around themselves
+        flipSpeed: 0.017,
+    };
+    const colors = [
+        {front: '#3B870A', back: '#235106'},
+        {front: '#B96300', back: '#6f3b00'},
+        {front: '#E23D34', back: '#88251f'},
+        {front: '#CD3168', back: '#7b1d3e'},
+        {front: '#664E8B', back: '#3d2f53'},
+        {front: '#394F78', back: '#222f48'},
+        {front: '#008A8A', back: '#005353'},
+    ];
+
+    setupCanvas();
+    updateConfetti();
+
+    confetti.addEventListener('click', addConfetti);
+    window.addEventListener('resize', () => {
+        setupCanvas();
+        hideConfetti();
+    });
+
+    // Confetti constructor
+    function Conf() {
+        this.randomModifier = rand(-1, 1);
+        this.colorPair = colors[Math.floor(rand(0, colors.length))];
+        this.dimensions = {
+            x: rand(confettiParams.size.x[0], confettiParams.size.x[1]),
+            y: rand(confettiParams.size.y[0], confettiParams.size.y[1]),
+        };
+        this.position = {
+            x: clickPosition[0],
+            y: clickPosition[1]
+        };
+        this.rotation = rand(0, 2 * Math.PI);
+        this.scale = {x: 1, y: 1};
+        this.velocity = {
+            x: rand(-confettiParams.initSpeed, confettiParams.initSpeed) * 0.4,
+            y: rand(-confettiParams.initSpeed, confettiParams.initSpeed)
+        };
+        this.flipSpeed = rand(0.2, 1.5) * confettiParams.flipSpeed;
+
+        if (this.position.y <= container.h) {
+            this.velocity.y = -Math.abs(this.velocity.y);
+        }
+
+        this.terminalVelocity = rand(1, 1.5) * confettiParams.terminalVelocity;
+
+        this.update = function () {
+            this.velocity.x *= 0.98;
+            this.position.x += this.velocity.x;
+
+            this.velocity.y += (this.randomModifier * confettiParams.drag);
+            this.velocity.y += confettiParams.gravity;
+            this.velocity.y = Math.min(this.velocity.y, this.terminalVelocity);
+            this.position.y += this.velocity.y;
+
+            this.scale.y = Math.cos((this.position.y + this.randomModifier) * this.flipSpeed);
+            this.color = this.scale.y > 0 ? this.colorPair.front : this.colorPair.back;
+        }
+    }
+
+    function updateConfetti() {
+        confettiCtx.clearRect(0, 0, container.w, container.h);
+
+        confettiElements.forEach((c) => {
+            c.update();
+            confettiCtx.translate(c.position.x, c.position.y);
+            confettiCtx.rotate(c.rotation);
+            const width = (c.dimensions.x * c.scale.x);
+            const height = (c.dimensions.y * c.scale.y);
+            confettiCtx.fillStyle = c.color;
+            confettiCtx.fillRect(-0.5 * width, -0.5 * height, width, height);
+            confettiCtx.setTransform(1, 0, 0, 1, 0, 0)
+        });
+
+        confettiElements.forEach((c, idx) => {
+            if (c.position.y > container.h ||
+                c.position.x < -0.5 * container.x ||
+                c.position.x > 1.5 * container.x) {
+                confettiElements.splice(idx, 1)
+            }
+        });
+        window.requestAnimationFrame(updateConfetti);
+    }
+
+    function setupCanvas() {
+        container = {
+            w: confetti.clientWidth,
+            h: confetti.clientHeight
+        };
+        confetti.width = container.w;
+        confetti.height = container.h;
+    }
+
+    function addConfetti(e) {
+        const canvasBox = confetti.getBoundingClientRect();
+        if (e) {
+            clickPosition = [
+                e.clientX - canvasBox.left,
+                e.clientY - canvasBox.top
+            ];
+        } else {
+            clickPosition = [
+                canvasBox.width * Math.random(),
+                canvasBox.height * Math.random()
+            ];
+        }
+        for (let i = 0; i < confettiParams.number; i++) {
+            confettiElements.push(new Conf())
+        }
+    }
+
+    function hideConfetti() {
+        confettiElements = [];
+        window.cancelAnimationFrame(updateConfetti)
+    }
+
+    function stopConfetti() {
+        confettiParams.number = 0;
+    }
+
+    function confettiLoop() {
+        setInterval(addConfetti, 700);
+        setTimeout(stopConfetti, 10000);
+    }
+
+    // Confetti code ends
+
+    // Create new wheel object specifying the parameters at creation time.
+    let theWheel = new Winwheel({
+        'outerRadius': 212,        // Set outer radius so wheel fits inside the background.
+        'innerRadius': 75,         // Make wheel hollow so segments don't go all way to center.
+        'textFontSize': 14,         // Set default font size for the segments.
+        'textOrientation': 'horizontal', // Make text curved.
+        'textAlignment': 'outer', // Align text to outside of wheel.
+        'textMargin': 8,
+        'textFontFamily': 'monospace',
+        'numSegments': 22,         // Specify number of segments.
+        'segments':             // Define segments including colour and text.
+            [                               // font size and test colour overridden on backrupt segments.
+                {'fillStyle': '#fff', 'text': 'Headphones', 'textFillStyle': '#000'},
+                {'fillStyle': '#e31119', 'text': 'Try Again', 'textFillStyle': '#000'},
+                {'fillStyle': '#009a3e', 'text': 'Umbrella', 'textFillStyle': '#000'},
+                {'fillStyle': '#e31119', 'text': 'Try Again', 'textFillStyle': '#000'},
+                {'fillStyle': '#fff', 'text': 'T-shirt', 'textFillStyle': '#000'},
+                {'fillStyle': '#e31119', 'text': 'Try Again', 'textFillStyle': '#000'},
+                {'fillStyle': '#009a3e', 'text': 'Mug', 'textFillStyle': '#000'},
+                {'fillStyle': '#e31119', 'text': 'Try Again', 'textFillStyle': '#000'},
+                {'fillStyle': '#fff', 'text': 'Notebook', 'textFillStyle': '#000'},
+                {'fillStyle': '#e31119', 'text': 'Try Again', 'textFillStyle': '#000'},
+                {'fillStyle': '#009a3e', 'text': 'Pen', 'textFillStyle': '#000'},
+                {'fillStyle': '#e31119', 'text': 'Try Again', 'textFillStyle': '#000'},
+                {'fillStyle': '#fff', 'text': 'Key ring', 'textFillStyle': '#000'},
+                {'fillStyle': '#e31119', 'text': 'Try Again', 'textFillStyle': '#000'},
+                {'fillStyle': '#009a3e', 'text': 'Shopping Voucher', 'textFontSize': 12, 'textFillStyle': '#000'},
+                {'fillStyle': '#e31119', 'text': 'Try Again', 'textFillStyle': '#000'},
+                {'fillStyle': '#fff', 'text': 'Water bottle', 'textFontSize': 12, 'textFillStyle': '#000'},
+                {'fillStyle': '#e31119', 'text': 'Try Again', 'textFillStyle': '#000'},
+                {'fillStyle': '#009a3e', 'text': 'Mbuzi', 'textFillStyle': '#000'},
+                {'fillStyle': '#e31119', 'text': 'Try Again', 'textFillStyle': '#000'},
+                {'fillStyle': '#fff', 'text': 'Hoodie', 'textFillStyle': '#000'},
+                {'fillStyle': '#e31119', 'text': 'Try Again', 'textFillStyle': '#000'},
+            ],
+        'animation':           // Specify the animation to use.
+            {
+                'type': 'spinToStop',
+                'duration': 10,    // Duration in seconds.
+                'spins': 3,     // Default number of complete spins.
+                'callbackFinished': alertPrize,
+                'callbackSound': playSound,   // Function to call when the tick sound is to be triggered.
+                'soundTrigger': 'pin'        // Specify pins are to trigger the sound, the other option is 'segment'.
+            },
+        'pins':				// Turn pins on.
+            {
+                'number': 22,
+                'fillStyle': 'silver',
+                'outerRadius': 4,
+            }
+    });
+
+    // Loads the tick audio sound in to an audio object.
+    let audio = new Audio('tick.mp3');
+
+    // This function is called when the sound is to be played.
+    function playSound() {
+        // Stop and rewind the sound if it already happens to be playing.
+        audio.pause();
+        audio.currentTime = 0;
+
+        // Play the sound.
+        audio.play();
+    }
+
+    // Vars used by the code in this page to do power controls.
+    let wheelPower = 0;
+    let wheelSpinning = false;
+
+    // -------------------------------------------------------
+    // Function to handle the onClick on the power buttons.
+    // -------------------------------------------------------
+    function powerSelected(powerLevel) {
+        // Ensure that power can't be changed while wheel is spinning.
+        if (wheelSpinning === false) {
+            // Reset all to grey incase this is not the first time the user has selected the power.
+            document.getElementById('pw1').className = "";
+            document.getElementById('pw2').className = "";
+            document.getElementById('pw3').className = "";
+
+            // Now light up all cells below-and-including the one selected by changing the class.
+            if (powerLevel >= 1) {
+                document.getElementById('pw1').className = "pw1";
+            }
+
+            if (powerLevel >= 2) {
+                document.getElementById('pw2').className = "pw2";
+            }
+
+            if (powerLevel >= 3) {
+                document.getElementById('pw3').className = "pw3";
+            }
+
+            // Set wheelPower var used when spin button is clicked.
+            wheelPower = powerLevel;
+
+            // Light up the spin button by changing it's source image and adding a clickable class to it.
+            document.getElementById('spin_button').src = "spin_on.png";
+            document.getElementById('spin_button').className = "clickable";
+        }
+    }
+
+    // -------------------------------------------------------
+    // Click handler for spin button.
+    // -------------------------------------------------------
+    function startSpin() {
+        // Ensure that spinning can't be clicked again while already running.
+        if (wheelSpinning === false) {
+            // Based on the power level selected adjust the number of spins for the wheel, the more times is has
+            // to rotate with the duration of the animation the quicker the wheel spins.
+            if (wheelPower === 1) {
+                theWheel.animation.spins = 3;
+            } else if (wheelPower === 2) {
+                theWheel.animation.spins = 6;
+            } else if (wheelPower === 3) {
+                theWheel.animation.spins = 10;
+            }
+
+            // Disable the spin button so can't click again while wheel is spinning.
+            document.getElementById('spin_button').src = "spin_off.png";
+            document.getElementById('spin_button').className = "";
+
+            // Begin the spin animation by calling startAnimation on the wheel object.
+            theWheel.startAnimation();
+
+            // Set to true so that power can't be changed and spin button re-enabled during
+            // the current animation. The user will have to reset before spinning again.
+            wheelSpinning = true;
+        }
+    }
+
+    // -------------------------------------------------------
+    // Function for reset button.
+    // -------------------------------------------------------
+    function resetWheel() {
+        stopConfetti();
+        prize.innerHTML = "";
+        prize.style.border = "";
+        theWheel.stopAnimation(false);  // Stop the animation, false as param so does not call callback function.
+        theWheel.rotationAngle = 0;     // Re-set the wheel angle to 0 degrees.
+        theWheel.draw();                // Call draw to render changes to the wheel.
+
+        document.getElementById('pw1').className = "";  // Remove all colours from the power level indicators.
+        document.getElementById('pw2').className = "";
+        document.getElementById('pw3').className = "";
+
+        wheelSpinning = false;          // Reset to false to power buttons and spin can be clicked again.
+    }
+
+    // -------------------------------------------------------
+    // Called when the spin animation has finished by the callback feature of the wheel because I specified callback in the parameters.
+    // -------------------------------------------------------
+    function alertPrize(indicatedSegment) {
+        // Just alert to the user what happened.
+        // In a real project probably want to do something more interesting than this with the result.
+        if (indicatedSegment.text !== 'Try Again') {
+            confettiParams.number = 70;
+            prize.innerHTML = `${indicatedSegment.text}!`;
+            confettiLoop();
+        } else {
+            stopConfetti();
+            prize.innerHTML = `Try Again`;
+        }
+    }
